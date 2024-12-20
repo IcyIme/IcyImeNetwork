1ac3edbba9e05c291d70b736f8df1ffbf180b10a
icyime <peter.odrobinak@outlook.sk>
2024-12-20 23:01:47 +0100

    localization

--- IcyImeNetwork/Program.cs
+++ IcyImeNetwork/Program.cs
@@ -4,23 +4,18 @@ using IcyImeNetwork;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
-using IcyImeNetwork.Services;
 using Microsoft.Extensions.Localization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddRadzenComponents();
 
 // Add localization and local storage services
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
-builder.Services.AddSingleton<IStringLocalizer, MultiResourceStringLocalizer>();
 builder.Services.AddBlazoredLocalStorage();
-
-builder.Services.AddScoped<LanguageService>();
-
-
-
+builder.Services.AddRadzenComponents();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+await host.SetDefaultCulture(); // Retrieves local storage value and sets the thread's current culture.
+await host.RunAsync();
