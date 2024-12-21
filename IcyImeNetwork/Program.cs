b104f8fa18be3230bdf3e8b1adffa529c5b76883
icyime <peter.odrobinak@outlook.sk>
2024-12-21 20:34:54 +0100

    .

--- IcyImeNetwork/Program.cs
+++ IcyImeNetwork/Program.cs
@@ -16,8 +16,7 @@ builder.Services.AddLocalization(options => options.ResourcesPath = "Resources")
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddRadzenComponents();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<BlazorTransitionableRoute.IRouteTransitionInvoker,
-    BlazorTransitionableRoute.DefaultRouteTransitionInvoker>();
+
 builder.Services.AddSingleton<LanguageShuffle>();
 
 var host = builder.Build();
